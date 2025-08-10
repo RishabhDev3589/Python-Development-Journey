2c962326b820635d206346a06940f0a5a5867f40
Rishabh S. Jha <rishabhsbjha9379@gmail.com>
2025-08-10 14:51:10 +0530

    Add files via upload
    A beginner-friendly Python script  that performs basic arithmetic operations(addition ,subtraction ,multiplication ,division ,modulus ,floor division and exponentiation) with user input and clear output.

---
+++ Basic_Calculator.py
@@ -0,0 +1,23 @@
+# Basic Calculator
+print("Welcome to Python World")
+print("Let's Make Arithmetical Operation")
+print("To Perform This Please Enter 2 values")
+num1=float(input("Enter Your 1st value:-"))
+num2=float(input("Enter Your 2nd value:-"))
+add=num1+num2
+sub=num1-num2
+multi=num1*num2
+div=num1/num2
+mod=num1%num2
+pure_div=num1//num2
+expo=num1**num2
+print("The Addition of Two Number is",add)
+print("The Subtraction of Two Number is",sub)
+print("The Multiplication of Two Number is",multi)
+print("The Division of Two Number is",div)
+print("The  Reminder of Two Number is",mod)
+print("The PureDivision of Two Number is",pure_div)
+print("The power of Two Number is",int(expo))
+print("Thanks Later")
+print("JBMNMJ")
+feedb=input("Give your feedback:-")
