c5a7588f10a7ba979a252abc77767d1e4d551e44
Rishabh S. Jha <rishabhsbjha9379@gmail.com>
2025-08-14 18:55:40 +0530

    Add files via upload
    A collection of Python Concept and programs with practice scripts covering core concepts like input/output, variables, data types & operators. Designed for learning, revision, and reference.

---
+++ Python_programs.py
@@ -0,0 +1,55 @@
+print("JBMNMJ")
+print('HELLO WORLD')
+
+#There are 5 Primary Datatype in Python :
+
+# 1.Integer (+ve,-ve,0) {Whole Values}
+age1=18
+age2=-18
+age3=0
+
+# 2.String {Sentence,Mix-up of words/values} (' '," ",''' ''')<-3 Way to put the value on string
+name1='Rishabh'
+name2="Rishabh"
+name3='''Rishabh'''
+print(name1,name2,name3)
+
+# 3.Float(Decimal Values)
+price=29.88
+
+# 4.Boolean (True,False) *(T,F are in capital letter)
+moonsoon=True
+summer=False
+
+# 5.None (Jis mey hamey koi bhi value abhi put nahi karna hai ussmey ham none put kar deytey hai ) *(N are in capital letter)
+a=None
+
+#datatype detection by Python:-
+print(type(age1))
+print(type(name1))
+print(type(price))
+print(type(moonsoon))
+print(type(a))
+
+#Python is case-sensitive language (A,a both are different)
+
+#Comments on Python->
+# 1.Single-Line comment
+''' 2.Multi-Line comment'''
+
+#Types of Operators in Python->
+
+# a+b-> (Here a,b is a Operand & + is a Operator )
+
+# 1.Arithmetic Operators (+,-,*,/,**,%)
+A=5
+B=8
+print(A+B , A-B , A*B , A/B , A%B , A**B)
+
+# IMP-> **(a to the power b)
+# % (it gives Reminder)
+# In Python if we divide 2 value we always get a value in floating
+ 
+# 2.Relational/Comparison Operators(==,!=,>,<,<=,>=)
+
+# Assignment Operator(=)
